4ed36aea8fd57e0c5505617fe0bd2fada069ccb9
angelggd <angelgabriel.gd@gmail.com>
2026-08-04 15:11:37 -0500

    feat(models) corregir error next

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -53,9 +53,9 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+  if (!this.isModified("password")) return next;
   this.password = await bcryptjs.hash(this.password, 10);
-  next();
+  next;
 });
 
 userSchema.methods.comparePassword = function (plain) {
