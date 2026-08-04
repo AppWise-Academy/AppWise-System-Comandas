00416ac9a47335ab37f0c961e3831e0a8887adf3
angelggd <angelgabriel.gd@gmail.com>
2026-08-04 15:20:24 -0500

    feat(routes): agregar validacion en el register

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { registerController } from "../controllers/user.controller.js";
+import {validateInput} from "../middlewares/validateInput.js"
+import { body } from "express-validator"
+import { ROLES } from "../shared/constants.js" 
 
 const router = Router();
 
-router.post("register", registerController);
+router.post("/register",
+  [
+    body("name").trim().isLength({min:3, max:50}).withMessage("name debe contener minimo 3 y maximo 50 caracteres"),
+    body("password").isLength({min:8}).withMessage("password debe contener minimo 8 caracteres"),
+    body("rol").trim().toLowerCase().isIn(Object.values(  ROLES)).withMessage(`Rol no valido. los Roles permitidos son ${Object.values(ROLES).join(', ')}`),
+    body("email").trim().isEmail().withMessage("email invalido")
+  ],
+  validateInput,
+  registerController);
 
 export default router;
