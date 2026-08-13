b8fa51a93014843edd6e683653b7196212c173eb
agus-stenico0 <agustinstenico58@gmail.com>
2026-08-13 18:04:26 -0300

    feat: Funcionalidad agregar items a una comanda existente

--- api/src/routes/comanda.js
+++ api/src/routes/comanda.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { Router } from 'express';
 import { 
-    createComanda
+    createComanda,
+    addItem
     
 } from '../controllers/comanda.controller.js';
 
 const router = Router()
 
 router.post('/comandas/', authGuard, roleGuard(['mozo', 'admin']), createComanda)
-
+router.post('/comandas/:id/items', authGuard, roleGuard(['mozo', 'admin']), addItem )
 
 export default router
  
