adf29d8df1e3d32e58439c2863ba0302d766edc3
agus-stenico0 <agustinstenico58@gmail.com>
2026-08-13 19:46:29 -0300

    Cambiar el estado de la comanda

--- api/src/routes/comanda.js
+++ api/src/routes/comanda.js
@@ -2,14 +2,14 @@ import express from 'express';
 import { Router } from 'express';
 import { 
     createComanda,
-    addItem
-    
+    addItem,
+    changeState
 } from '../controllers/comanda.controller.js';
 
 const router = Router()
 
 router.post('/comandas/', authGuard, roleGuard(['mozo', 'admin']), createComanda)
 router.post('/comandas/:id/items', authGuard, roleGuard(['mozo', 'admin']), addItem )
-
+router.put('/comandas/:id/estado', authGuard, roleGuard(['mozo', 'admin', 'cajero']), changeState)
 export default router
  
