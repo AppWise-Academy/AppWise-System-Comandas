bcfbbccb1d2ddae73371f752a6bee37b07ca3486
angelggd <angelgabriel.gd@gmail.com>
2026-08-14 16:44:32 -0500

    feat(middleware): agregar authguard validar token agregar roleguard validar rol

---
+++ api/src/middlewares/authGuard.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken')
+
+function authGuard(req, res, next){
+
+  // 1- leer el header authorization
+  const authHeader = req.headers["authorization"];
+
+  if(!authHeader || !authHeader.startsWith("Bearer ")){
+    return res.status(401).json({
+      ok: false,
+      error: "Token requerido. Envia: Authorozation: Bearer TOKEN",
+    });
+  }
+
+  // 2. Extraer el token
+  const token = authHeader.split(" ")[1];
+
+  //3. Verificar el token
+  try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // {userId, email, rol, iat, exp}
+    next();
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({
+        ok:false,
+        error: "Token Expirado",
+        code: "TOKEN_EXPIRED",
+      });
+    }
+
+    return res.status(401).json({
+      ok: false,
+      error: "Token inválido",
+      code: "TOKEN_INVALID",
+    });
+  }
+}
+
+module.exports = authGuard;
